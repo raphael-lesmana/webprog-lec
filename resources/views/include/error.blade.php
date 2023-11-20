da9cade05842bb7c4b846cbdf58af5a45c2bda32
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2023-11-20 14:03:06 +0700

    missing files

---
+++ resources/views/include/error.blade.php
@@ -0,0 +1,14 @@
+@if ($errors->any())
+    <div class="row justify-content-center">
+        <div class="col-4 z-4 position-absolute">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+@endif
