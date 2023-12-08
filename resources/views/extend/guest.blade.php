c64e3e0045a5ac9731f1d18a99d70c59fbadac59
yenniferwilanata <yenniferwilanata@gmail.com>
2023-12-08 21:22:32 +0700

    login

--- resources/views/extend/guest.blade.php
+++ resources/views/extend/guest.blade.php
@@ -10,10 +10,10 @@
         @section('error')
             @include('include.error')
         @show
-        <div class="grid z-3">
-            <div class="row d-flex">
-                <div class="col-8 guest-background"><img src="/assets/logo.png" alt=""></div>
-                <div class="col-4 mx-auto" style="width:450px; margin-top: 150px; color:black;">
+        <div class="grid">
+            <div class="row">
+                <div class="col-8 guest-background"><img src="/assets/logo.png" alt="" style="object-fit:cover"></div>
+                <div class="col-4 d-flex" style="color:black; justify-content: center; align-items: center">
                     @section('content')
                     @show
                 </div>
