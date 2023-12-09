5033ba4386580ca465559cc0a7544ddcc90302f9
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2023-12-09 09:02:51 +0700

    fix url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
                 <div class=row>
                     @foreach ($capsules as $item)
                     <div class="col-4" style="margin-left: 10px; margin-right: 10px">
-                        <a href="/item/item/{{$item->id}}">
+                        <a href="/item/{{$item->id}}">
                         <button class="btn btn-light active" type="button" style="background-color: #CED9BB; margin-top: 10px; margin-bottom: 10px; border-radius: 25px">
                             <img src="/storage/assets/items/{{$item->picture}}" style="height: 200px; width: 300px; object-fit: cover; border-radius: 25px">
                             <p style="margin: auto; font-size: 20px">{{$item->name}}</p>
